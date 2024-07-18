6719d5b03651e44aeec0ef297db579770190d745
Fares ghalwash <117339225+mrfa0gh@users.noreply.github.com>
2024-07-18 08:00:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# make sure install instaloader
-#by using pip install instaloader
-
-
-
 import instaloader
 import re
 import os
@@ -13,17 +8,16 @@ def get_instagram_info(username):
     try:
         profile = instaloader.Profile.from_username(loader.context, username)
 
-        # Download profile picture
         loader.download_profilepic(profile)
 
-        # Gather the required information
         user_info = {
             "Name": profile.full_name,
             "Username": profile.username,
             "Followers": profile.followers,
             "Following": profile.followees,
             "Posts": profile.mediacount,
-            "Is Private": profile.is_private
+            "Is Private": profile.is_private,
+            "Bio": profile.biography
         }
 
         return user_info
@@ -31,25 +25,32 @@ def get_instagram_info(username):
         return None
 
 def save_user_info(username, user_info):
-    # Save user info to a text file
-    with open(f"{username}.txt", "w", encoding="utf-8") as file:
+    dir_name = username
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+
+    info_file_path = os.path.join(dir_name, f"{username}.txt")
+    with open(info_file_path, "w", encoding="utf-8") as file:
         for key, value in user_info.items():
-            file.write(f"{key}: {value}\n")
+            if key != "Bio":  
+                file.write(f"{key}: {value}\n")
+
+    bio_file_path = os.path.join(dir_name, f"bio{username}.txt")
+    with open(bio_file_path, "w", encoding="utf-8") as bio_file:
+        bio_file.write(user_info["Bio"])
 
 def main():
-    # Ask the user for input
     input_username = input("Enter the Instagram username (with or without '@'): ")
 
-    # Remove '@' if present
     username = re.sub(r'^@', '', input_username)
 
-    # Get user info
     user_info = get_instagram_info(username)
 
     if user_info:
         save_user_info(username, user_info)
-        print(f"User information saved in {username}.txt")
-        print(f"Profile picture saved as {username}.png")
+        print(f"User information saved in {username}/{username}.txt")
+        print(f"User bio saved in {username}/bio{username}.txt")
+        print(f"Profile picture saved as {username}/{username}.png")
     else:
         print("User not found.")
 
